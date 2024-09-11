17ce3981dc9d84bdbe178570a635b69a22903012
Kevin Wang <kevmo314@gmail.com>
2024-09-11 02:13:41 -0400

    feat: implement sections 4.11 to 4.14

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,23 @@
-#![allow(non_snake_case)]  // easier to match the C API.
+#![allow(non_snake_case)] // easier to match the C API.
+
+use nvml_wrapper_sys::bindings::nvmlReturn_t;
+
 #[tarpc::service]
 pub trait Scuda {
-    async fn nvmlInitWithFlags(flags: u32) -> u32;
+    // 4.11 Initialization and Cleanup
+    async fn nvmlInitWithFlags(flags: u32) -> nvmlReturn_t;
+    async fn nvmlInit_v2() -> nvmlReturn_t;
+    async fn nvmlShutdown() -> nvmlReturn_t;
+
+    // 4.12 Error reporting
+    async fn nvmlErrorString(result: nvmlReturn_t) -> String;
+
+    // 4.14 System Queries
+    async fn nvmlSystemGetCudaDriverVersion() -> (nvmlReturn_t, i32);
+    async fn nvmlSystemGetCudaDriverVersion_v2() -> (nvmlReturn_t, i32);
+    async fn nvmlSystemGetDriverVersion(length: u32) -> (nvmlReturn_t, String);
+    async fn nvmlSystemGetNVMLVersion(length: u32) -> (nvmlReturn_t, String);
+    async fn nvmlSystemGetProcessName(pid: u32, length: u32) -> (nvmlReturn_t, String);
 
     async fn nvml_device_get_name(device: u64, length: u32) -> String;
 
