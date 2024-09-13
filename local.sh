63520fadde73d67394877e474a1fae30807aaecd
Brodey Newman <brodeynewman@gmail.com>
2024-09-13 04:18:58 +0000

    chore: sh updates

--- local.sh
+++ local.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+libscuda_path="$(pwd)/libscuda.so"
+client_path="$(pwd)/client.cu"
+
 build() {
-  echo "rebuilding client..."
-  cargo build
-}
+  echo "building client..."
 
-server() {
-  echo "starting server..."
-  build
-
-  cargo run --features=disable-preload
-}
-
-run() {
-  build
-  local libscuda_path="$(pwd)/target/debug/libscuda.so"
-
-  echo "Full path to libscuda.so: $libscuda_path"
+  if [[ "$(uname)" == "Linux" ]]; then
+    nvcc -shared -Xcompiler -fPIC -o $libscuda_path $client_path -lcudart
+  else
+    echo "No compiler options set for os "$(uname)""
+  fi
 
   if [ ! -f "$libscuda_path" ]; then
       echo "libscuda.so not found. build may have failed."
       exit 1
   fi
+}
+
+server() {
+  echo "starting server..."
+}
+
+run() {
+  build
+
 
   LD_PRELOAD="$libscuda_path" nvidia-smi
 }
