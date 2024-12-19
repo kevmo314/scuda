2d744fd2b1a5e15fd1d5cada50c64edf502f8938
Brodey Newman <brodeynewman@gmail.com>
2024-12-19 03:54:59 +0000

    chore: manual server

--- codegen/manual_server.h
+++ codegen/manual_server.h
@@ -2,9 +2,11 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+int handle_cudaFree(void *conn);
 int handle_cudaMemcpy(void *conn);
 int handle_cudaMemcpyAsync(void *conn);
 int handle_cudaLaunchKernel(void *conn);
+int handle_cudaMallocManaged(void *conn);
 int handle___cudaRegisterVar(void *conn);
 int handle___cudaRegisterFunction(void *conn);
 int handle___cudaRegisterFatBinary(void *conn);
